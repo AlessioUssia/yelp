4f237ab8c3a07070f5e82f2d8e5f2aaaa5a3bcd8
AlessioUssia <s282376@studenti.polito.it>
2024-04-10 14:20:42 +0200

    primo commit

---
+++ database/business.py
@@ -0,0 +1,34 @@
+from dataclasses import dataclass
+from database.review import Review
+
+
+@dataclass
+class Business:
+    business_id: str
+    full_address: str
+    active: str
+    categories: str
+    city: str
+    review_count: int
+    business_name: str
+    neighborhoods: str
+    latitude: float
+    longitude: float
+    state: str
+    stars: float
+
+    reviews_id: list[str]
+    reviews = list[Review] = None
+
+    def __eq__(self, other):
+        return self.business_id == other.business_id
+
+    def __hash__(self):
+        return hash(self.business_id)
+
+    def get_reviews(self):
+        if self.reviews is None:
+            # vado a leggerle dal DAO e popolo la lista
+            dao.get_reviews(self.reviews_id)
+        else:
+            return self.reviews
